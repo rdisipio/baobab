6711fb9914ede9341f93fe7a797152a271f037aa
rdisipio <disipio@cern.ch>
2016-05-24 21:26:08 +0200

    adding run

--- runBaobab.py
+++ runBaobab.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python
 
+import os, sys
+import argparse
+
+from ROOT import *
+from root_numpy import *
+
+parser = argparse.ArgumentParser(description='ttbar all-hadronic analyzer')
+parser.add_argument( '-i', '--input_filelist', help="Input file list", default="" )
+parser.add_argument( '-n', '--nevents',        help="Maximum number of events", type=int, default=-1 )
+parser.add_argument( '-o', '--output_filename', help="Output file name", default="output.histograms.root" )
+parser.add_argument( '-s', '--systematic', help='systematic uncertainty', default="nominal" )
+parser.add_argument( '-t', '--truth', action="store_true", help='Run in truth mode' )
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+   print """
+              v .   ._, |_  .,
+           `-._\/  .  \ /    |/_
+               \\  _\, y | \//
+         _\_.___\\, \\/ -.\||
+           `7-,--.`._||  / / ,
+           /'     `-. `./ / |/_.'
+                     |    |//
+                     |_    /
+                     |-   |
+                     |   =|
+                     |    |
+--------------------/ ,  . \--------._
+INFO: Running Baobab
+"""
